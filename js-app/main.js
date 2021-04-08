53261f4b49df8d9a27bf66117d17e3862b3de679
Tatiane Komrij <tatiane.b.komrij@gmail.com>
2021-04-08 15:20:34 -0500

    fix to fetch call

--- js-app/main.js
+++ js-app/main.js
@@ -1,7 +1,8 @@
-const url = "https://localhost:5001/api/beanvariety/";
+const beanVarietyUrl = "https://localhost:5001/api/beanvariety/";
+const coffeeUrl = "https://localhost:5001/api/coffee/";
 
-const button = document.querySelector("#run-button");
-button.addEventListener("click", () => {
+const beanButton = document.querySelector("#run-beanVariety-button");
+beanButton.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
             console.log(beanVarieties);
@@ -9,5 +10,51 @@ button.addEventListener("click", () => {
 });
 
 function getAllBeanVarieties() {
-    return fetch(url).then(resp => resp.json());
+    return fetch(beanVarietyUrl).then(resp => resp.json());
 }
+
+//-----------------------------------------------------------
+
+const coffeeButton = document.querySelector("#run-coffee-button");
+coffeeButton.addEventListener("click", () => {
+    getAllCoffees()
+        .then(coffees => {
+            console.log(coffees);
+        })
+});
+
+function getAllCoffees() {
+    return fetch(coffeeUrl).then(resp => resp.json());
+}
+
+
+//------------------------------------------------------------
+
+const addBeanButton = document.querySelector("#run-addBean-button");
+addBeanButton.addEventListener("click", () => {
+    const name = document.querySelector("#name").value
+    const region = document.querySelector("#region").value
+    console.log("beans")
+
+    const newBean = {
+        name,
+        region
+    }
+    console.log(newBean)
+
+    function addBean(newBean) {
+        return fetch(beanVarietyUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newBean)
+        }).then(getAllBeanVarieties);
+    }
+
+
+    addBean(newBean)
+        .then(beanVarieties => {
+            console.log(beanVarieties)
+        })
+})
